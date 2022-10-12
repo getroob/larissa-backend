d426e426da8e1a73810e978c967285c2320d9864
e.myriounis <el.myrioun@gmail.com>
2022-10-12 22:18:44 +0300

    update

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ import express from "express";
 import cors from "cors";
 import listEndpoints from "express-list-endpoints";
 
-import serverRouter from "./routes/server.js";
-import userRouter from "./routes/user.js";
-import errorHandler from "./errorHandler.js";
-import sequelize, { testDB } from "./db/index.js";
-import formRouter from "./routes/form.js";
-import appointmentRouter from "./routes/appointment.js";
-import User from "./db/models/user.js";
-import encryptPassword from "./tools/encryptPassword.js";
+import serverRouter from "./src/routes/server.js";
+import userRouter from "./src/routes/user.js";
+import errorHandler from "./src/errorHandler.js";
+import sequelize, { testDB } from "./src/db/index.js";
+import formRouter from "./src/routes/form.js";
+import appointmentRouter from "./src/routes/appointment.js";
+import User from "./src/db/models/user.js";
+import encryptPassword from "./src/tools/encryptPassword.js";
 
 const server = express();
 server.use(
